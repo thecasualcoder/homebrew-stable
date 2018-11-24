f24486bb90d2cc755a47f5f893410c1fcd63460e
Aswin Karthik <aswinkarthik93@gmail.com>
2018-11-25 00:18:34 +0530

    Update to version 1.1.1

--- Formula/tztail.rb
+++ Formula/tztail.rb
@@ -1,9 +1,9 @@
 class Tztail < Formula
     desc "tail with timezones"
     homepage "https://github.com/thecasualcoder/tztail"
-    url "https://github.com/thecasualcoder/tztail/releases/download/1.1.0/tztail_1.1.0_darwin_amd64.tar.gz"
-    sha256 "42df063dd5072aa4ea753e533554c8cff4845c6a2523c46b127e15022752c1cf"
-    version "1.1.0"
+    url "https://github.com/thecasualcoder/tztail/releases/download/1.1.1/tztail-1.1.1-x86_64-darwin.tar.gz"
+    sha256 "656aef0b1bb7a6646cdcf1367710559a0fa713d7ae5559545e92dc339a9a69fa"
+    version "1.1.1"
     version_scheme 1
     def install
       bin.install "tztail"

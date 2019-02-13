e9b78c364da7cd003b03cdcc3619262d0c560ff9
Arunvel Sriram <arunvelsriram@gmail.com>
2019-02-14 00:09:45 +0530

    kube-tmuxp v0.2.3

--- Formula/kube-tmuxp.rb
+++ Formula/kube-tmuxp.rb
@@ -1,9 +1,9 @@
 class KubeTmuxp < Formula
   desc "Tool to generate tmuxp configs that help to switch between multiple Kubernetes contexts safely"
   homepage "https://github.com/thecasualcoder/kube-tmuxp"
-  version "0.2.2"
+  version "0.2.3"
   url "https://github.com/thecasualcoder/kube-tmuxp/releases/download/v#{version}/kube-tmuxp_#{version}_Darwin_x86_64.tar.gz"
-  sha256 "87b40bbf40e87c3f00e84ee0751b2885545541805f15abbd13bfe412225c7ebb"
+  sha256 "26b16343f6042e70fa165244682d4af1cd86dfff59c1cb5d306079a465c3b6b7"
   def install
     bin.install "kube-tmuxp"
   end

9eda5be99e7c76207979edeaea33b267e28cf275
Aswin Karthik <aswinkarthik93@gmail.com>
2019-02-26 23:35:55 +0530

    Add Formula for pg-ping

---
+++ Formula/pg-ping.rb
@@ -0,0 +1,14 @@
+class PgPing < Formula
+    desc "Ping your postgres continously"
+    homepage "https://github.com/thecasualcoder/pg-ping"
+    version "0.1.0"
+    url "https://github.com/thecasualcoder/pg-ping/archive/v#{version}.tar.gz"
+    
+    depends_on "go" => :recommended
+
+    def install
+        ENV['PGPING_VERSION'] = "v#{version}"
+        system "make", "compile"
+        bin.install "pg-ping"
+    end
+end

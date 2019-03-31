6c5800649c0dd582a5dcf01cac47ce75a7b528c7
Arunvel Sriram <arunvelsriram@gmail.com>
2019-03-31 23:25:08 +0530

    Downgrade kube-tmuxp to 0.2.3

--- Formula/kube-tmuxp.rb
+++ Formula/kube-tmuxp.rb
@@ -1,7 +1,7 @@
 class KubeTmuxp < Formula
   desc "Tool to generate tmuxp configs that help to switch between multiple Kubernetes contexts safely"
   homepage "https://github.com/thecasualcoder/kube-tmuxp"
-  version "0.2.4"
+  version "0.2.3"
   url "https://github.com/thecasualcoder/kube-tmuxp/releases/download/v#{version}/kube-tmuxp_#{version}_Darwin_x86_64.tar.gz"
   sha256 "26b16343f6042e70fa165244682d4af1cd86dfff59c1cb5d306079a465c3b6b7"
   def install

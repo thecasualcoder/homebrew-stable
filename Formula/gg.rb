a2113d7d12df895d04df9356918da41520d4da5f
Dinesh <dineshudt17@gmail.com>
2019-08-11 23:01:41 +0530

    [Dinesh] Add sha256 key for gg

--- Formula/gg.rb
+++ Formula/gg.rb
@@ -2,6 +2,7 @@ class Gg < Formula
   desc "A tool to manage multiple git repositories"
   homepage "https://github.com/thecasualcoder/gg"
   url "https://github.com/thecasualcoder/gg/releases/download/0.1.0/gg-0.1.0-x86_64-darwin.tar.gz"
+  sha256 "1c358814a76ce0b4ecfd67a1e4487b706299592b756c35603860aba6722650c9"
   version "0.1.0"
   def install
     bin.install "gg"

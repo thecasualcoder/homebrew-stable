e0d629e54989f0c05d42e4f12f06fc8aa12cd469
Aswin Karthik <aswinkarthik93@gmail.com>
2019-10-08 23:03:45 +0530

    Release v1.3.0 of csvdiff

--- Formula/csvdiff.rb
+++ Formula/csvdiff.rb
@@ -1,11 +1,11 @@
 class Csvdiff < Formula
   desc "A Blazingly fast diff tool for comparing csv files"
   homepage "https://github.com/aswinkarthik/csvdiff"
-  sha256 "7363c4062a7569a85179b2e74aa7e6e1917e18bc984aa7b19ec1add2c83129fd"
-  version "1.2.0"
+  sha256 "f43be419615fbdded509fd27102b9f88e3b14284b5717576949f46af5027ac05"
+  version "1.3.0"
   url "https://github.com/aswinkarthik/csvdiff/archive/v#{version}.tar.gz"
 
-  depends_on "go" => :recommended
+  depends_on "go" => :build
 
   def install
     system "go",

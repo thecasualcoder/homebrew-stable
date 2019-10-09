d949bfe59659d0b7053c0236e8e12afdacdcb950
Ninan John <jninan@thoughtworks.com>
2019-10-09 09:54:53 +0530

    Update lazy-connect formula to v1.6.0

--- Formula/lazy-connect.rb
+++ Formula/lazy-connect.rb
@@ -1,8 +1,8 @@
 class LazyConnect < Formula
   desc "Shell function to fuzzy search an IPSec VPN by name and connect to it automatically."
   homepage "https://github.com/thecasualcoder/lazy-connect"
-  url "https://github.com/thecasualcoder/lazy-connect/archive/v1.5.0.zip"
-  sha256 "5bc394efe7dddfe9df9d9d81cd4a738e347912c5ad7eca4a7e70ad8ae822a218"
+  url "https://github.com/thecasualcoder/lazy-connect/archive/v1.6.0.zip"
+  sha256 "77dfcbca7a605ac32081b03aebaed3a3e3182f65cddaa1204306615b2fc84a8a"
   depends_on "fzf" => :recommended
   depends_on "oath-toolkit" => :recommended
   def install

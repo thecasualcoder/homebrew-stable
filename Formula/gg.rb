5ef603a04caaa4def3c20f0442dfcce9750f9af9
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-12-09 18:29:39 +0530

    gg v0.1.1

--- Formula/gg.rb
+++ Formula/gg.rb
@@ -1,9 +1,9 @@
 class Gg < Formula
   desc "A tool to manage multiple git repositories"
   homepage "https://github.com/thecasualcoder/gg"
-  url "https://github.com/thecasualcoder/gg/releases/download/0.1.0/gg-0.1.0-x86_64-darwin.tar.gz"
-  sha256 "1c358814a76ce0b4ecfd67a1e4487b706299592b756c35603860aba6722650c9"
-  version "0.1.0"
+  url "https://github.com/thecasualcoder/gg/releases/download/0.1.1/gg-0.1.1-x86_64-darwin.tar.gz"
+  sha256 "b252372ab667897d8c497c8423ab0be0d81fa13cad9c105a61b974c3cbe4b338"
+  version "0.1.1"
   def install
     bin.install "gg"
   end

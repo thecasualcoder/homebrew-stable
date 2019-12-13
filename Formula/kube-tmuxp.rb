d30c835d4fe685d60d03e42d3c2b3e2c80f0216d
Dinesh <dineshudt17@gmail.com>
2019-12-13 11:55:12 +0530

    [Dinesh] Update kube-tmuxp to 0.2.6
    Signed-off-by: Dinesh <dineshudt17@gmail.com>

--- Formula/kube-tmuxp.rb
+++ Formula/kube-tmuxp.rb
@@ -1,7 +1,7 @@
 class KubeTmuxp < Formula
   desc "Tool to generate tmuxp configs that help to switch between multiple Kubernetes contexts safely"
   homepage "https://github.com/thecasualcoder/kube-tmuxp"
-  version "0.2.4"
+  version "0.2.6"
   url "https://github.com/thecasualcoder/kube-tmuxp/releases/download/v#{version}/kube-tmuxp_#{version}_Darwin_x86_64.tar.gz"
   sha256 "dbecc642ea58786eed95ba207063cccb696074cb1b7e80199e83dfe9937c6918"
   def install

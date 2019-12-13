9de5ff7b110cbd3b0cd1040ca0030ca08ca9d2a5
Dinesh <dineshudt17@gmail.com>
2019-12-13 14:12:07 +0530

    [Dinesh] Update sha256 for kube-tmuxp
    Signed-off-by: Dinesh <dineshudt17@gmail.com>

--- Formula/kube-tmuxp.rb
+++ Formula/kube-tmuxp.rb
@@ -3,7 +3,7 @@ class KubeTmuxp < Formula
   homepage "https://github.com/thecasualcoder/kube-tmuxp"
   version "0.2.6"
   url "https://github.com/thecasualcoder/kube-tmuxp/releases/download/v#{version}/kube-tmuxp_#{version}_Darwin_x86_64.tar.gz"
-  sha256 "dbecc642ea58786eed95ba207063cccb696074cb1b7e80199e83dfe9937c6918"
+  sha256 "8ef7aa20ec7186f6cc7f7457978ca94f62685eb49a1c892f4b6cb6709bdec815"
   def install
     bin.install "kube-tmuxp"
   end

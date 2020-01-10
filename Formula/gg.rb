fcd35b46aeb7c2897b7f6c08223cac950fa52298
Ninan John <jpninanjohn@gmail.com>
2020-01-10 08:59:04 +0530

    Update gg

--- Formula/gg.rb
+++ Formula/gg.rb
@@ -1,13 +1,13 @@
 class Gg < Formula
   desc "A tool to manage multiple git repositories"
   homepage "https://github.com/thecasualcoder/gg"
-  url "https://github.com/thecasualcoder/gg/releases/download/0.1.1/gg-0.1.1-x86_64-darwin.tar.gz"
-  sha256 "b252372ab667897d8c497c8423ab0be0d81fa13cad9c105a61b974c3cbe4b338"
-  version "0.1.1"
+  url "https://github.com/thecasualcoder/gg/releases/download/0.2.1/gg-0.2.1-x86_64-darwin.tar.gz"
+  sha256 "e1bb5231bd198c537c6404a6b18f9f37bcafcda32241c648da915fd3e6a8cd96"
+  version "0.2.1"
   def install
     bin.install "gg"
   end
   test do
-    assert_match "Git Governance 1.0", shell_output("#{bin}/gg --version")
+    assert_match "Git Governance 0.2.1", shell_output("#{bin}/gg --version")
   end
 end

b9a096a06cb32f5d5b9bb2fba68b767a9ce85dc7
Ninan John <jpninanjohn@gmail.com>
2020-01-23 00:08:47 +0530

    Fix version

--- Formula/gg.rb
+++ Formula/gg.rb
@@ -3,7 +3,7 @@ class Gg < Formula
   homepage "https://github.com/thecasualcoder/gg"
   url "https://github.com/thecasualcoder/gg/releases/download/0.3.0/gg-0.3.0-x86_64-darwin.tar.gz"
   sha256 "b6ecbe3afb58f51b9127d3cf0ce3e74c283173f2f2c9691aa97ff444fdc03c0b"
-  version "0.2.1"
+  version "0.3.0"
   def install
     bin.install "gg"
   end

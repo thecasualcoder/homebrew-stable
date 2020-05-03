49bcf81f4ad71977d9185b002bcfd1112e1b725b
Ninan John <jpninanjohn@gmail.com>
2020-05-03 10:56:18 +0530

    Update gg formula

--- Formula/gg.rb
+++ Formula/gg.rb
@@ -1,9 +1,9 @@
 class Gg < Formula
   desc "A tool to manage multiple git repositories"
   homepage "https://github.com/thecasualcoder/gg"
-  url "https://github.com/thecasualcoder/gg/releases/download/0.3.1/gg-0.3.1-x86_64-darwin.tar.gz"
+  url "https://github.com/thecasualcoder/gg/releases/download/0.3.2/gg-0.3.2-x86_64-darwin.tar.gz"
   sha256 "ed094bba103c062f695f47ab8f5730f8db18920f349fad856773381b02c8e9e1"
-  version "0.3.1"
+  version "0.3.2"
   def install
     bin.install "gg"
   end

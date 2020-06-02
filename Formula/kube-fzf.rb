a63d6982d587fc545d7ac1c2e896f6bbb56e40ec
Arunvel Sriram <arunvelsriram@gmail.com>
2020-06-02 22:24:07 +0530

    Update kube-fzf to v1.6.2

--- Formula/kube-fzf.rb
+++ Formula/kube-fzf.rb
@@ -1,8 +1,8 @@
 class KubeFzf < Formula
   desc "Fuzzy search over Kubernetes Pods from the command-line"
   homepage "https://github.com/thecasualcoder/kube-fzf"
-  url "https://github.com/thecasualcoder/kube-fzf/archive/v1.6.1.tar.gz"
-  sha256 "7c166523b1c0f54b4a6741366348778dbdf68063cd73282e39728436280c90d2"
+  url "https://github.com/thecasualcoder/kube-fzf/archive/v1.6.2.tar.gz"
+  sha256 "3722689339cf19fafeb46ae333a0b7c9e6d0e4eede0d8ab8c232eff899502c68"
   depends_on "fzf" => :recommended
   def install
     bin.install "kube-fzf.sh"

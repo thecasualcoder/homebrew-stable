1a5f16b72929c28177d43c81c67f6e7c04851b42
Dinesh <dineshudt17@gmail.com>
2020-11-13 21:45:10 +0530

    Add Stevedore formulae
    Co-authored-by: tharun208 <rajendrantharun@live.com>

---
+++ Formula/stevedore.rb
@@ -0,0 +1,24 @@
+class Stevedore < Formula
+  desc "Tool to load the cluster with containers for kubernetes to orchestrate"
+  homepage "https://github.com/gojekfarm/stevedore"
+  license "Apache-2.0"
+  head "https://github.com/gojekfarm/stevedore.git"
+  sha256 "ceb6a86560937ba1592e2b199e037acfdd07cf39ced34ad66e9a9b773b18e7a8"
+
+  bottle :unneeded
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "compile"
+    bin.install "out/stevedore"
+  end
+
+  test do
+    assert_match "Stevedore loads the cluster with containers for kubernetes to orchestrate",
+    shell_output("#{bin}/stevedore")
+
+    assert_match "Command failed: required flag(s) \"manifests-path\" not set",
+    shell_output("#{bin}/stevedore apply 2>&1", 1)
+  end
+end

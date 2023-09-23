a1a7591e240b9bdc58cb3dd7eb5dece8849a298e
Dinesh B <dineshudt17@gmail.com>
2023-09-23 11:13:04 +0530

    Update the version of tf-summarize to 0.3.3
    Signed-off-by: Dinesh B <dineshudt17@gmail.com>

--- Formula/tf-summarize.rb
+++ Formula/tf-summarize.rb
@@ -2,9 +2,10 @@ class TfSummarize < Formula
   desc "A command-line utility to print the summary of the terraform plan"
   homepage "https://github.com/dineshba/tf-summarize"
   license "The MIT License (MIT)"
-  version "0.2.2"
+  version "0.3.3"
   url "https://github.com/dineshba/tf-summarize.git", :tag => "v#{version}"
-  sha256 "b7de3f0c1b0504cf58c25758818d0c2604718ce7da0e3204f86024285417e934"
+  # sha256 is generated using: curl -sL https://github.com/dineshba/tf-summarize/archive/refs/tags/v0.3.3.tar.gz | shasum -a 256 | cut -d " " -f 1
+  sha256 "a42249825500cacaced32f108bfd12161e27608d2d160aa95deb24a8f91e89d1"
   depends_on "go" => :build
 
   def install
